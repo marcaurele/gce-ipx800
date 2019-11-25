60d47a4e2ac9605a64dfc2a04fd053aeaad4a7e7
Marc-Aurèle Brothier <m@brothier.org>
2019-11-25 18:31:52 +0100

    version 0.1

--- ipx800/__init__.py
+++ ipx800/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 *-*
 
-"""Module to control the IPX800 device from GCE Electronics."""
+"""Module to control the IPX800 V4 device from GCE Electronics."""
 
 from ipx800.ipx800 import ApiError, ipx800, relay  # noqa
 
 
-__version__ = "0.1rc1"
+__version__ = "0.1"
