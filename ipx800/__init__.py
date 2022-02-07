790098d806c057d5a54ecc6891b56d0c74d3ce1c
Marc-Aurèle Brothier <m@brothier.org>
2022-02-07 17:45:37 +0100

    bump version

--- ipx800/__init__.py
+++ ipx800/__init__.py
@@ -1,8 +1,6 @@
 """Module to control the IPX800 V4 device from GCE Electronics."""
 
-import importlib.metadata
-
 from ipx800.ipx800 import ApiError, IPX800 as ipx800  # noqa
 
 
-__version__ = importlib.metadata.version("gandi-2-terraform")
+__version__ = "0.5.0"
