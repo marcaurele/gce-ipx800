6f6dbc932feeb16d28ae3508f76fa3af9ead18de
Marc-Aurèle Brothier <m@brothier.org>
2022-02-07 17:26:17 +0100

    🎨 read version from package metadata

--- ipx800/__init__.py
+++ ipx800/__init__.py
@@ -1,6 +1,8 @@
 """Module to control the IPX800 V4 device from GCE Electronics."""
 
+import importlib.metadata
+
 from ipx800.ipx800 import ApiError, IPX800 as ipx800  # noqa
 
 
-__version__ = "0.5.dev0"
+__version__ = importlib.metadata.version("gandi-2-terraform")
